1700a51cc6d319e18869b454c3037108e8ac752e
DvirDukhan <dvir@redislabs.com>
2020-01-05 17:55:28 +0200

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
+
+requirements = list(map(str.strip, open("requirements.txt").readlines()))
+
 setup(
     name='gearsclient',
     version='0.1',
     packages=find_packages(),
-    install_requires=[
-        'redis',
-        'cloudpickle'
-    ],
+    install_requires=requirements,
 )
