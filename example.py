c41e65166724b19dc75c8ca4c0a564f34687b98f
meir@redislabs.com <meir@redislabs.com>
2020-11-01 20:56:50 +0200

    Fix LGTM warnings

--- example.py
+++ example.py
@@ -1,21 +1,8 @@
 from gearsclient import GearsRemoteBuilder as GRB
-from gearsclient import log, hashtag, execute, atomic
+from gearsclient import execute, atomic
 import redis
 
 conn = redis.Redis(host='localhost', port=6379)
-
-# count for each genre how many times it appears
-
-# class test:
-#     def __init__(self):
-#         self.count = 1
-
-# def func1(x):
-#     return test()
-
-# def func2(x):
-#     x.count += 1
-#     return x
 
 def func(x):
     with atomic():
@@ -28,8 +15,3 @@ res = GRB('ShardsIDReader',r=conn).\
       run()
 
 print(res)
-# if len(res[1]) > 0:
-#     print(res[1])
-
-# for r in res[0]:
-#     print('%s' % str(r.count))
